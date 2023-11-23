002df4c7e16af29c3432519741e70e8d7b6aeb50
Ste79ITA <nastri.s@gmail.com>
2023-11-23 16:53:47 +0100

    added input form and password length recovery.

--- index.php
+++ index.php
@@ -7,5 +7,23 @@
 </head>
 <body>
     
+<form action="" method="GET">
+    <input type="text" name="password_length"> Inserisci il numero di caratteri della password
+    <input type="submit">
+</form>
+
+<?php 
+
+$password_length = $_GET['password_length'] ?? 0;
+
+$new_password = '';
+
+function generate_password($password_length, &$new_password) {
+
+    var_dump($password_length);
+}
+
+generate_password($password_length, $new_password);
+?>
 </body>
 </html>
