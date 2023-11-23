f42f52e38a4692a29eb27c6326cc69be315aa235
Ste79ITA <nastri.s@gmail.com>
2023-11-23 17:15:45 +0100

    added resete and print password

--- index.php
+++ index.php
@@ -7,11 +7,6 @@
 </head>
 <body>
     
-<form action="" method="GET">
-    <input type="text" name="password_length"> Inserisci il numero di caratteri della password
-    <input type="submit">
-</form>
-
 <?php 
 
 $password_length = $_GET['password_length'] ?? 0;
@@ -20,15 +15,21 @@ $new_password = [];
 
 function generate_password($password_length, &$new_password) {
 
-    var_dump($password_length);
-
     for ($i = 0; $i < $password_length; $i++) {
-        echo $new_password[] = 1;
+        $new_password[] = 1;
 
     }
 }
 
 generate_password($password_length, $new_password);
 ?>
+
+<form action="" method="GET">
+    <input type="text" name="password_length" placeholder="<?= $password_length?>"> Inserisci il numero di caratteri della password
+    <input type="submit">
+    <input type="submit" value="Reset">
+</form>
+
+<h2>La tua password é : <?= implode($new_password)?> </h1>
 </body>
 </html>
